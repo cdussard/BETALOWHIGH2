460fabed6d7f8c05ec6dde7731d94194ff5f5cc1
cdussard <cdussard@ensc.fr>
2024-10-30 16:36:47 +0100

    ajout go no go EEG

---
+++ lua_files/6b_gonogo.lua
@@ -0,0 +1,34 @@
+
+function initialize(box)
+
+	dofile(box:get_config("${__volatile_ScenarioDir}/lua_files/lua-stimulator-stim-codes.lua"))
+
+	baseline_duration = box:get_setting(2)
+    pause_duration = box:get_setting(3)
+	-- initializes random seed
+	math.randomseed(os.time())
+
+end
+
+function process(box)
+
+	local t = 0
+	
+	t = t + 2
+		-- start xp
+	box:send_stimulation(1, OVTK_StimulationId_ExperimentStart, t, 0)
+	
+	t = t + 3
+	box:send_stimulation(1, OVTK_StimulationId_LabelStart, t, 0)
+	
+	for i = 1,50 do
+		box:send_stimulation(1, OVTK_StimulationId_SegmentStop, t, 0)
+	
+		t = t +2
+
+	end
+	
+	box:send_stimulation(1, OVTK_StimulationId_ExperimentStop, t, 0)
+	
+	
+end
