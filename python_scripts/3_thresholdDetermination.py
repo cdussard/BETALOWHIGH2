e7f1730ca0663aee05f407d13fe8cfd2785aa767
Lily-prr <liluxpereira@gmail.com>
2025-02-17 15:59:31 +0100

    modif 3_threshold

--- python_scripts/3_thresholdDetermination.py
+++ python_scripts/3_thresholdDetermination.py
@@ -9,13 +9,21 @@ import numpy as np
 import os
 
 #path = "C:\manips\BETALOWHIGH\developpement_pilote/csv_files/"
-path = "C:/Users/claire.dussard/OneDrive - ICM/Bureau/exp_highLowBeta/0_pilote/devpilote_BETALOWHIGH/csv_files/"
+path = "C:/Users/lilux/desktop/BETALOWHIGH2/csv_files/"
 sep = ","
 print(os.getcwd())
 rel_path = "../csv_files/"
 
-csv_filename = os.path.join(path, '5_tableau_moyennes.csv')
-tableau_echantillons = pd.read_csv(csv_filename)
+#csv_filename = os.path.join(path, '5_tableau_moyennes.csv') #tableur où sont récup les données csv write openvibe
+#tableau_echantillons = pd.read_csv(csv_filename)
+
+# Chargement des fichiers rest_target et rest_parasite
+csv_filename_target = os.path.join(path, 'rest_target.csv')
+csv_filename_parasite = os.path.join(path, 'rest_parasite.csv')
+
+tableau_echantillons_target = pd.read_csv(csv_filename_target)
+tableau_echantillons_parasite = pd.read_csv(csv_filename_parasite)
+
 
 #IL FAUT AUSSI LIRE LES VALEURS DU BETA PARASITE
 
@@ -26,26 +34,30 @@ percentilePireBeta = instructions["percentilePireBeta"]
 percentileMeilleurBeta = instructions["percentileMeilleurBeta"]
 
 
+echantillons_target = tableau_echantillons_target.iloc[:, 2]
+echantillons_parasite = tableau_echantillons_parasite.iloc[:, 2]
+print("nb échantillons target : " + str(len(echantillons_target)))
+print("nb échantillons parasite : " + str(len(echantillons_parasite)))
 
-echantillons = tableau_echantillons.iloc[:,2]
-print("nb echantillons : "+str(len(echantillons)))
+#echantillons = tableau_echantillons.iloc[:,2]
+#print("nb echantillons : "+str(len(echantillons)))
 #a priori faire 25th percentile sur laisser stabiliser beta 1 min puis 
 #1 min de repos  ==> voir dans quelle direction on a ajusté les valeurs
-min_val = echantillons.min()
-percentile_5 = np.percentile(echantillons, 5)
-percentile_10 = np.percentile(echantillons, 10)
-percentile_25 = np.percentile(echantillons, 25)
-percentile_47 = np.percentile(echantillons,47)
-percentile_50 = np.percentile(echantillons,50)
-percentile_75 = np.percentile(echantillons,75)
-percentile_pireBeta = np.percentile(echantillons,percentilePireBeta)
-percentile_meilleurBeta = np.percentile(echantillons,percentileMeilleurBeta)
-percentile_pireBetaParasite = #COMPLETER avec les valeurs du beta parasite (tableur ecrit par l'openvibe threshold)
-percentileMeilleurBeta_parasite = #COMPLETER 
+min_val = echantillons_target.min()
+percentile_5 = np.percentile(echantillons_target, 5)
+percentile_10 = np.percentile(echantillons_target, 10)
+percentile_25 = np.percentile(echantillons_target, 25)
+percentile_47 = np.percentile(echantillons_target,47)
+percentile_50 = np.percentile(echantillons_target,50)
+percentile_75 = np.percentile(echantillons_target,75)
+percentile_pireBeta = np.percentile(echantillons_target,percentilePireBeta)
+percentile_meilleurBeta = np.percentile(echantillons_target,percentileMeilleurBeta)
+percentile_pireBetaParasite = np.percentile(echantillons_parasite, percentilePireBeta) #COMPLETER avec les valeurs du beta parasite (tableur ecrit par l'openvibe threshold)
+percentileMeilleurBeta_parasite =  np.percentile(echantillons_parasite, percentileMeilleurBeta) #COMPLETER 
 
-max_val = echantillons.max()
+max_val = echantillons_target.max()
 
-stdev = round(np.std(echantillons),3)
+stdev = round(np.std(echantillons_target),3)
 
 
 dict_threshold = {
